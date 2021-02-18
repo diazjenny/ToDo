d4f2320b02d60350ed342b2a9d8e4344481344bd
Nikolokoto <59317741+Nikolokoto@users.noreply.github.com>
2021-02-18 19:45:09 -0400

    formulario funcionando con delay

--- src/screens/AddTask/components/Form/index.js
+++ src/screens/AddTask/components/Form/index.js
@@ -1,34 +1,39 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import "./form.scss"
 
-import {api} from '../../../../utils'
+import { api } from '../../../../utils'
 
-const Form = () =>{
+const Form = () => {
 
-    const [task,setTask] = useState({})
-    const [title,setTitle] = useState("")
-    const [date,setDate] = useState("")
-    const [description,setDescription] = useState("")
-    const [assignated,setAssignated] = useState("")
+    const [task, setTask] = useState({})
+    const [title, setTitle] = useState("")
+    const [date, setDate] = useState("")
+    const [description, setDescription] = useState("")
+    const [assigned, setAssigned] = useState("")
 
     const submitForm = (e) => {
         e.preventDefault()
-        setTask({title})
+        setTask({
+            title: title,
+            date: date,
+            description: description,
+            assigned: assigned
+        });
         console.log(task)
     }
 
     return (
-        <form onSubmit =  {(e)=>submitForm(e)}>
-            <input type="text" name="title" onChange={(e)=>setTitle(e.target.value)} ></input>
-            <input type="date" name="date"></input>
-            <textarea name="description"></textarea>
-            <select name="assignated">
+        <form onSubmit={(e) => submitForm(e)}>
+            <input type="text" name="title" onChange={(e) => setTitle(e.target.value)} ></input>
+            <input type="date" name="date" onChange={(e) => setDate(e.target.value)}></input>
+            <textarea name="description" onChange={(e) => setDescription(e.target.value)}></textarea>
+            <select name="assigned" defaultValue="assigned to" onChange={(e) => setAssigned(e.target.value)}>
                 <option disabled>Assign to</option>
                 <option value="Ailen">Ailen </option>
                 <option value="Nico">Nico </option>
                 <option value="Flor">Flor </option>
                 <option value="Jenny">Jenny </option>
-            </select> 
+            </select>
             <button type="submit">Add Task</button>
         </form>
     )
